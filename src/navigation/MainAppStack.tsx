554404ad1090c987be533a225b97905e3b4df890
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-05-02 01:09:13 +0100

    working on eccomerce app, updated cart layout

--- src/navigation/MainAppStack.tsx
+++ src/navigation/MainAppStack.tsx
@@ -1,19 +1,30 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import AuthStack from "./AuthStack";
 import MainAppBottomTabs from "./MainAppBottomTabs";
+import MyOrdersScreen from "../screens/profile/MyOrdersScreen";
+import CheckoutScreen from "../screens/cart/CheckoutScreen";
 
-
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function MainAppStack() {
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false
-            }}
-        >
-            <Stack.Screen name="AuthStack" component={AuthStack}/>
-            <Stack.Screen name="MainAppBottomTabs" component={MainAppBottomTabs} />
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="AuthStack" component={AuthStack} />
+      <Stack.Screen name="MainAppBottomTabs" component={MainAppBottomTabs} />
+      <Stack.Screen
+        name="CheckoutScreen"
+        options={{ headerShown: true }}
+        component={CheckoutScreen}
+      />
+      <Stack.Screen
+        name="MyOrdersScreen"
+        options={{ headerShown: true, title: "My Orders" }}
+        component={MyOrdersScreen}
+      />
+    </Stack.Navigator>
+  );
 }
--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,12 +1,30 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import HomeHeader from "../../components/headers/HomeHeader";
 import AppSaveView from "../../components/views/AppSaveView";
+import HomeHeader from "../../components/headers/HomeHeader";
+import { sharedPaddingHorizontal } from "../../styles/sharedStyles";
+import AppText from "../../components/texts/AppText";
+import { s, vs } from "react-native-size-matters";
+import { useNavigation } from "@react-navigation/native";
+import ProfileSectionButton from "../../components/buttons/ProfileSectionButton";
 
 const ProfileScreen = () => {
+  const navigation = useNavigation();
+  
   return (
     <AppSaveView>
       <HomeHeader />
+      <AppText variant="bold" style={{ fontSize: s(18), marginTop: vs(10) }}>
+        Hello, Ahmed
+      </AppText>
+      <View style={{ paddingHorizontal: sharedPaddingHorizontal }}>
+        <ProfileSectionButton
+          title={"My Orders"}
+          onPress={() => navigation.navigate("MyOrdersScreen")}
+        />
+        <ProfileSectionButton title={"Language"} />
+        <ProfileSectionButton title={"Logout"}/>
+      </View>
     </AppSaveView>
   );
 };
